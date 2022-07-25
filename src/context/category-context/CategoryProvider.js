2c1e9164dc09040642d6b0b35aff0a78852a615a
rasool700 <rasoolkarami700@gmail.com>
2022-07-25 21:04:33 +0430

    return to context

--- src/context/category-context/CategoryProvider.js
+++ src/context/category-context/CategoryProvider.js
@@ -1,19 +1,22 @@
-import React, {
-  useContext,
-  createContext,
-  useReducer,
-  useEffect,
-  useState,
-} from "react";
+import React, { useContext, createContext, useReducer, useEffect } from "react";
 import CategoryReducer from "./CategoryReducer";
 const CategoryContext = createContext();
 const CategoryContextDispatcher = createContext();
 const CategoryProvider = ({ children }) => {
-  const [categories, setCategories] = useState([]);
   const initialValue = {
     categories: [],
     total: 0,
   };
+  // useEffect(() => {
+  //   var existingEntries = JSON.parse(localStorage.getItem("category"));
+  //   if (existingEntries == null) existingEntries = [];
+  //   console.log(existingEntries);
+  //   if (existingEntries.length > 1) {
+  //     dispatch({ type: "LOAD_CATEGORIES", payload: existingEntries });
+  //   } else {
+  //     return;
+  //   }
+  // }, []);
 
   const [category, dispatch] = useReducer(CategoryReducer, initialValue);
 
